6042a6baa833d63d5b195fce770ad5dda080ca08
Almir Jakubaitis <almir@icone3.com.br>
2020-07-12 02:14:12 -0300

    first commit

--- .git-actions.sh
+++
@@ -1,8 +0,0 @@
-echo "# Github_Explorer" >> README.md
-git init
-git add README.md
-git add .
-git add -A
-git commit -m "first commit"
-git remote add origin https://github.com/almirjakubaitis/Github_Explorer.git
-git push -u origin master
